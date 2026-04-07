5b6939d82eb95db3d2e7aafc8ae6e2716e532a67
andrzejpolednik <andrew1991@interia.eu>
2026-04-07 19:49:05 +0200

    Add parameterized test

--- src/test/java/DemoBlaze/PurchaseTests.java
+++ src/test/java/DemoBlaze/PurchaseTests.java
@@ -4,6 +4,8 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 
 public class PurchaseTests extends BaseTests{
@@ -12,29 +14,31 @@ public class PurchaseTests extends BaseTests{
     String confirmationSuccess = "Thank you for your purchase!";
 
 
-
+    @ParameterizedTest
+    @CsvSource({
+            "Laptops, Sony vaio i5, Andrzej, 123456",
+            "Laptops, MacBook air, Marek, 987654",
+            "Phones, Samsung galaxy s6, Jan, 111222"
+    })
     @Epic("DemoBlaze")
     @Feature("Shopping process")
-    @Test
-    public void purchaseShouldEndWithConfirmationText(){
+    public void purchaseShouldEndWithConfirmationText(String productCategory, String productName, String name, String creditCard ){
 
         HomePage homePage = new HomePage(driver, wait);
 
         homePage.go();
 
-        homePage.selectCategory("Laptops");
+        homePage.selectCategory(productCategory);
 
-        ProductPage productPage = homePage.selectProduct("Sony vaio i5");
+        ProductPage productPage = homePage.selectProduct(productName);
 
         productPage.addToCart();
 
         CartPage cartPage = productPage.goToCart();
 
-        Assertions.assertTrue(cartPage.isProductInCart("Sony vaio i5"));
+        Assertions.assertTrue(cartPage.isProductInCart(productName));
 
-
-
-        cartPage.placeOrder("Andrzej", "Poland", "Cracow", "123456", "12", "2027");
+        cartPage.placeOrder(name, "Poland", "Cracow", creditCard, "12", "2027");
 
         Assertions.assertEquals(confirmationSuccess, driver.findElement(By.cssSelector("div.sa-custom + h2")).getText());
 
